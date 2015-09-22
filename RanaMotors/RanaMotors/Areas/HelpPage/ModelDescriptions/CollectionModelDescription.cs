c580a30f64e5b48d39a1b41457f1ddfd7b49946c
Naresh Mekala <nareshm.net@gmail.com>
2015-09-22 19:14:30 +0100

    First commit into GITHUB

---
+++ RanaMotors/RanaMotors/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace RanaMotors.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
